1902eba58d0b473d31b04c35db0fa0e14adfc2f5
Kyle Tse <shtse8@gmail.com>
2025-12-10 13:51:29 +0000

    style: format package.json files with biome

--- types/wasm.d.ts
+++ types/wasm.d.ts
@@ -1,10 +1,10 @@
 // Type declarations for WASM modules
 declare module '*.wasm' {
-	const content: string
-	export default content
+  const content: string
+  export default content
 }
 
 declare module 'tree-sitter-wasms/out/*.wasm' {
-	const content: string
-	export default content
+  const content: string
+  export default content
 }
